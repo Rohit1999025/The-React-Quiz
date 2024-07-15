ed3abe3bed93915426ddca6cfaa47d2b18c0f721
Rohit Chauhan <97845005+Rohit1999025@users.noreply.github.com>
2024-07-15 23:11:55 +0530

    Add files via upload

---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,27 @@
+function FinishedScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = ({ points } / { maxPossiblePoints }) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🥈";
+  if (percentage >= 60 && percentage < 80) emoji = "😎";
+  if (percentage >= 40 && percentage < 60) emoji = "🤔";
+  if (percentage < 40) emoji = "🤦";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You Scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(percentage)}%);
+      </p>
+      <p className="highscore">(Highscore : {highscore} Points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishedScreen;
